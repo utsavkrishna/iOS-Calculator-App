deee99d619ada9053442d91b90720f4202412829
UTSAV KRISHNA PANDEY <utsavkrishnapandey@gmail.com>
2021-07-04 21:48:46 +0530

    Add files via upload

---
+++ CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Utsav Krishna.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
